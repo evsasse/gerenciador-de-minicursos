c44ccac63eda6f02e7d8acc19eb5b9941fdcf47c
Evandro Sasse <evsasse@gmail.com>
2016-06-10 00:07:27 +0900

    solution to show/hide editing ui

--- client/EditarMinicurso.jsx
+++ client/EditarMinicurso.jsx
@@ -1,20 +1,25 @@
 import React, {Component} from 'react';
+import classNames from 'classnames';
 
 export default class EditarMinicurso extends Component{
   render(){
-    const { minicurso } = this.props;
+    const minicurso = (this.props.minicurso)?
+      this.props.minicurso : { _id: '', nome: '', descricao: '', ativo: false};
+
     const inativo = (minicurso.ativo)? '':'inativo';
+    const editarCriar = (minicurso._id)? 'Editar':'Criar';
     return(
-      <form className="minicurso editar-minicurso">
+      <form className={classNames("minicurso", "editar-minicurso", this.props.className)}>
+        { this.props.children }
         <div className="editar-minicurso-main">
           <span>
-            <input type="text" defaultValue={minicurso.nome}/>
+            <input type="text" defaultValue={minicurso.nome} placeholder="Nome"/>
             <label><input type="checkbox" defaultChecked={minicurso.ativo}/>Ativo</label>
           </span>
-          <input type="submit" value="Editar"/>
+          <input type="submit" value={editarCriar}/>
         </div>
         <div>
-          <textarea defaultValue={ minicurso.descricao }/>
+          <textarea defaultValue={ minicurso.descricao } placeholder="Descrição, pode conter Markdown"/>
         </div>
       </form>
     )
--- client/GerenciarMinicursos.jsx
+++ client/GerenciarMinicursos.jsx
@@ -4,21 +4,43 @@ import Minicurso from "./Minicurso.jsx";
 import EditarMinicurso from "./EditarMinicurso.jsx";
 
 export default class GerenciarMinicursos extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      editarAtual: ''
+    }
+  }
+
+  mostrarEditarMinicurso(id){
+    id = (id === this.state.editarAtual)? '': id;
+    this.setState({
+      editarAtual: id
+    })
+  }
+
   render(){
     let { minicursos } = this.context;
     minicursos = minicursos.map(
-      (cur)=>(
-        <div key={cur._id}>
-          <Minicurso minicurso={ cur }/>
-          <EditarMinicurso minicurso={ cur }/>
-        </div>
-      )
+      (cur)=>{
+        let esconder = (this.state.editarAtual === cur._id)?'':'editar-esconder';
+        return (
+          <div key={cur._id}>
+            <Minicurso minicurso={ cur }>
+              <button onClick={this.mostrarEditarMinicurso.bind(this,cur._id)}>Editar</button>
+            </Minicurso>
+            <EditarMinicurso minicurso={ cur } className={esconder}/>
+          </div>
+        )
+      }
     )
 
     return(
       <div>
         <h1>#GerenciarMinicursos</h1>
         <div>{ minicursos }</div>
+        <EditarMinicurso>
+          <h2>Novo Minicurso</h2>
+        </EditarMinicurso>
       </div>
     )
   }
